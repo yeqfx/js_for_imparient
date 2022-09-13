774132136d1b5f4527c1b491f8689f885c05888a
yeqfx <yeqfx@hotmail.com>
2022-09-13 15:10:44 +0000

    Chapter 1

---
+++ code/01.ts
@@ -0,0 +1,14 @@
+// typeof 가 반환하는 형식은?
+console.log(typeof(typeof 42));
+
+// 변수를 초기화하지 않을 때 할당되는 값은?
+let x;
+console.log(typeof x);
+
+// 어떤 수를 0으로 나누면?
+console.log(10 / 0);
+console.log(-10 / 0);
+console.log(0 / 0);
+
+// 숫자를 반환하는 함수의 입력값에 오류가 있을 때 반환하는 값은?
+console.log(parseInt('1pie'));
